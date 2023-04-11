6bf3be3b1d9dcd8aa83399af8efbf426d4c6a067
arif-k32 <arif3681@gmail.com>
2023-04-11 16:48:32 +0530

    removed unnecessary services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { IsloggedinService } from './services/isloggedin.service';
 import { TokenstorageService } from './services/tokenstorage.service';
 
 @Component({
@@ -11,15 +10,8 @@ export class AppComponent {
   title = 'route';
   constructor(
     private router: Router,
-    public isloggedinService: IsloggedinService,
     private tokenStorage: TokenstorageService
   ) {}
 
-  logout() {
-    // if(this.isloggedinService.isloggedin){
-    this.isloggedinService.isloggedin = false;
-    this.tokenStorage.accessToken = '';
-    this.router.navigate(['home']);
-    // }
-  }
+ 
 }
--- src/app/login/root/root.component.ts
+++ src/app/login/root/root.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpServiceService } from 'src/app/services/http-service.service';
-import { IsloggedinService } from 'src/app/services/isloggedin.service';
 import { TokenstorageService } from 'src/app/services/tokenstorage.service';
 
 @Component({
@@ -13,7 +12,6 @@ export class RootComponent {
   constructor(
     private router: Router,
     private http: HttpServiceService,
-    private isloggedinService: IsloggedinService,
     private tokenStorage: TokenstorageService
   ) {}
 
@@ -30,7 +28,6 @@ export class RootComponent {
     let users: any[];
     this.http.login(this.loginform).subscribe((response: any) => {
       this.tokenStorage.accessToken = response.access_token;
-      this.isloggedinService.isloggedin = true;
       localStorage.setItem(
         'access_token',
         JSON.stringify(response.access_token)
