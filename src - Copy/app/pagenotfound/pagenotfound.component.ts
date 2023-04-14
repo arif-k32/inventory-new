1d43fb0e82d910b5482cb9ed213346d43bc6e0f3
arif-k32 <arif3681@gmail.com>
2023-04-14 08:52:00 +0530

    folder restructuring

--- src - Copy/app/pagenotfound/pagenotfound.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pagenotfound',
-  templateUrl: './pagenotfound.component.html',
-})
-export class PagenotfoundComponent {}
--- src/app/Shared/Interfaces/products/products.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IProducts {
-    active:boolean,
-    id:number,
-    name:string,
-    price:number,
-    sku:string,
-    stock:number,
-}
